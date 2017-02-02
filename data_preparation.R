5f8896f99ac7ced37180231600e11c42406746c8
Олег Шмелев <oleshm@gmail.com>
2017-02-02 16:13:43 +0400

    Добавлена таблица с расшифровкой индекса дисциплин

--- data_preparation.R
+++ data_preparation.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readxl)
+library(stringr)
 
 # competence --------------------------------------------------------------
 
@@ -38,3 +39,41 @@ library(readxl)
 # rm(list = ls(pattern = "^competence"))
 
 competence <- read_csv("data/DB_competence.csv")
+
+# subject -----------------------------------------------------------------
+
+
+subject <- bind_rows(
+  read_excel(
+    "data/БД 38.03.06 ТД Коммерция.xls",
+    sheet = 3,
+    col_names = c("id", "name", "kathedra_id")
+  ) %>%
+    mutate(profile = "Коммерция"),
+  read_excel(
+    "data/БД 38.03.06 ТД Логистика.xls",
+    sheet = 3,
+    col_names = c("id", "name", "kathedra_id")
+  ) %>%
+    mutate(profile = "Логистика")
+) %>%
+  bind_rows(
+read_excel(
+  "data/БД 38.03.06 ТД Маркетинг.xls",
+  sheet = 3,
+  col_names = c("id", "name", "kathedra_id")
+) %>%
+  mutate(profile = "Маркетинг")
+)
+
+# subject %>%
+#   select(id) %>% arrange(id) %>% distinct() %>%
+#   separate(id, c("x1", "x2", "x3", "x4", "x5"), remove = FALSE) %>%
+#   write_excel_csv("data/subject_id.csv")
+
+subject_id <- read_csv("data/subject_id.csv")
+
+subject_id %>%
+  unite(nid, - id, sep = ".", remove = FALSE) %>%
+  mutate(nid = str_replace_all(nid, "\\.NA", "")) %>%
+  View()
